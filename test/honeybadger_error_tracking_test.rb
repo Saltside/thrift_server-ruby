d97bf8d2372ad92a75ecccf74b135c449442e23c
ahawkins <adam@hawkins.io>
2015-02-25 19:12:18 +0100

    Send context to HB

--- test/honeybadger_error_tracking_test.rb
+++ test/honeybadger_error_tracking_test.rb
@@ -2,11 +2,18 @@ require_relative 'test_helper'
 
 class HoneybadgerErrorTrackerTest < MiniTest::Unit::TestCase
   def test_logs_according_to_honeybadger_api
+    rpc = ThriftServer::RPC.new :name, [ :args ]
+
     client = mock
-    client.expects(:notify_or_ignore).with(:error)
+    client.expects(:notify_or_ignore).with(:error, context: {
+      rpc: {
+        name: rpc.name,
+        args: rpc.args.inspect
+      }
+    })
 
     tracker = ThriftServer::HoneybadgerErrorTracker.new client
 
-    tracker.track :rpc, :error
+    tracker.track rpc, :error
   end
 end
