b6383b0c4d277bf6e0c7026b8e9910c50aeb8f0b
ahawkins <adam@hawkins.io>
2015-03-04 09:46:34 +0100

    Don't use RPC name for progname
    This produces more readable logs. In most formatters the RPC name will
    be on the same column on each line.

--- lib/thrift_server/log_subscriber.rb
+++ lib/thrift_server/log_subscriber.rb
@@ -3,31 +3,31 @@ module ThriftServer
     include Concord.new(:logger)
 
     def server_start(server)
-      logger.info 'server' do
+      logger.info :server do
         "Started on port %d" % [ server.port ]
       end
 
-      logger.info 'server' do
+      logger.info :server do
         "-> Threads: %d" % [ server.threads ]
       end
 
-      logger.info 'server' do
+      logger.info :server do
         "-> Transport: %s" % [ server.transport ]
       end
 
-      logger.info 'server' do
+      logger.info :server do
         "-> Protocol: %s" % [ server.protocol ]
       end
     end
 
     def server_thread_pool_change(meta)
-      logger.debug 'server' do
+      logger.debug :server do
         "Thread pool change: %+d" % [ meta.fetch(:delta) ]
       end
     end
 
     def server_connection_opened(addr)
-      logger.debug 'server' do
+      logger.debug :server do
         "%s:%d connected" % [
           addr.ip_address,
           addr.ip_port
@@ -36,7 +36,7 @@ module ThriftServer
     end
 
     def server_connection_closed(addr)
-      logger.debug 'server' do
+      logger.debug :server do
         "%s:%d disconnected" % [
           addr.ip_address,
           addr.ip_port
@@ -45,14 +45,18 @@ module ThriftServer
     end
 
     def rpc_ok(rpc, response, meta)
-      logger.info rpc.name do
-        "OK (%.2fms)" % [ meta.fetch(:latency) ]
+      logger.info :processor do
+        "%s => OK (%.2fms)" % [
+          rpc.name,
+          meta.fetch(:latency)
+        ]
       end
     end
 
     def rpc_error(rpc, ex, meta)
-      logger.info rpc.name do
-        "Error! %s (%.2fms)" % [
+      logger.info :processor do
+        "%s => Error! %s (%.2fms)" % [
+          rpc.name,
           ex.class.name,
           meta.fetch(:latency)
         ]
@@ -62,8 +66,9 @@ module ThriftServer
     end
 
     def rpc_exception(rpc, ex, meta)
-      logger.info rpc.name do
-        "%s (%.2fms)" % [
+      logger.info :processor do
+        "%s => %s (%.2fms)" % [
+          rpc.name,
           rpc.exception_name(ex),
           meta.fetch(:latency)
         ]
